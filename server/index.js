d62f1c404f215978aa7662c32e1a39992251e46a
Javed Baloch <mjavedahmed@live.com>
2022-03-01 20:27:56 +0500

    Redux added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ dotenv.config()
 const app = express()
 
 // Routing
-app.use('/posts', postsRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
@@ -18,6 +17,9 @@ app.use(cors())
 
 const CONNECTION_URL = process.env.MONGO_DB_URI
 const PORT = process.env.PORT || 5000
+
+app.use('/posts', postsRoutes)
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then( () => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
