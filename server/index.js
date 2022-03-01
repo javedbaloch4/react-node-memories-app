7091cd98599383bdc625512a6459659584a13e44
Javed Baloch <mjavedahmed@live.com>
2022-03-01 14:21:45 +0500

    Basic setup

--- server/index.js
+++ server/index.js
@@ -8,3 +8,12 @@ const app = express()
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cros())
+
+const CONNECTION_URL = process.env.MONGODB_URI
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTpoplogy: true })
+    .then( () => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
